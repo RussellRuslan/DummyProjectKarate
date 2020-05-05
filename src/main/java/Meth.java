184b712d7fd9bdd262753d02f04f77ef365e433e
RussellRuslan <SamatovR@aetna.com>
2020-05-05 08:03:50 -0400

    fr tr

--- src/main/java/Meth.java
+++ src/main/java/Meth.java
@@ -1,5 +1,7 @@
 public class Meth {
     public static void main(String[] args) {
         System.out.println("Hi there ");
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println(1+8);
     }
 }
